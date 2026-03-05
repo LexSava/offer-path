5acab1e1886647f42637cdea2302630ef1022148
Alexei Savostyanov <lexsava@gmail.com>
2026-03-05 09:15:49 +0100

    refactor: Move modals to common/modals; add prisma scripts
    Reorganize modal components by moving login-modal.tsx and create-application-modal.tsx into src/components/common/modals/ and adding a new barrel export (src/components/common/modals/index.ts). Update src/components/common/index.ts to re-export the modals via export * from './modals' and remove the previous individual modal exports. Also add npm scripts: postinstall (prisma generate) and prisma:generate to ensure Prisma artifacts are generated after install.

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,4 @@
-export { LoginModal } from './login-modal';
-export { CreateApplicationModal } from './create-application-modal';
 export { Input } from './input/input';
 export { Dropdown } from './dropdown/dropdown';
 export { Textarea } from './textarea/textarea';
+export * from './modals';
---
+++ src/components/common/modals/index.ts
@@ -0,0 +1,2 @@
+export { CreateApplicationModal } from './create-application-modal';
+export { LoginModal } from './login-modal';
